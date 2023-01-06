1b309bbec9bbee76cfd17ab23074a29328df72f4
Davide <me@davegabe.it>
2023-01-06 17:17:30 +0100

    Small fix on introduction section of the report

--- analize_dataset.py
+++ analize_dataset.py
@@ -19,7 +19,7 @@ def plot_csv(path):
     data['Total Images'] = data['Train Images'] + data['Validation Images']
 
     # Plot "Common Name" and "Train Images" count and "Validation Images" on top of it
-    fig, ax = plt.subplots(figsize=(13, 7))
+    fig, ax = plt.subplots(figsize=(13, 5.5))
     ax.barh(data['Common Name'], data['Train Images'], color='blue')
     ax.barh(data['Common Name'], data['Validation Images'], left=data['Train Images'], color='orange')
     ax.set_xlabel('Number of Images')
